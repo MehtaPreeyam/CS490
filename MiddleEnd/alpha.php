cfedb65a1133d2d107ec73f2f5c1a8454ab0e392
Nikhil Vummadi <nrv26@njit.edu>
2018-09-23 16:52:38 -0400

    Communication between front and back end

--- MiddleEnd/alpha.php
+++ MiddleEnd/alpha.php
@@ -1,11 +1,11 @@
 <?php
 
-$json_data = file_get_contents();
-$data = json_decode($json_data, TRUE);
+$json_data = file_get_contents("php://input");
+$data = json_decode($json_data, true);
 $username = $data['username']
 $password = $data['password']
 
-
+$arr = array('username' => $username, 'password' => $password); 
 
 $ch = curl_init();
 
@@ -14,14 +14,11 @@ curl_setopt_array($ch, array(
 	CURLOPT_URL => "https://web.njit.edu/~sk972/alpha.php",
 	CURLOPT_USERAGENT => "POST Request from middle",
 	CURLOPT_POST => 1,
-	CURLOPT_POSTFIELDS => array(
-		username => 'username',
-		password => 'password'
-		)
+	CURLOPT_POSTFIELDS => json_encode($arr)
 ));
 
-$response = curl_exec($ch);
+$response = json_decode(curl_exec($ch), true);
 
 curl_close($ch);
-echo $response;
->
+echo json_encode($response);
+?>
