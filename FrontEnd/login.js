8f1125ee6de821ad80ace4e307059dc7a8f1bd1c
Preeyam Mehta <preeyammehta12@gmail.com>
2018-09-24 00:53:09 -0400

    f

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -6,19 +6,23 @@ function validate() {
     'use strict';
     var username = document.getElementById("username").value,
         password = document.getElementById("password").value,
-        userPass = "username=" + username + "&password=" + password,
+        userPass = Object.create(null),
         http = new XMLHttpRequest();
-
+    userPass.username = username;
+    userPass.password = password;
+    console.log(userPass);
     http.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             var json = JSON.parse(this.responseText);
-            if (json.valid === 'valid') {
-                alert("Login successfully");
+            console.log(json);
+            alert(json.username);
+            if (json.username === 'ppm24') {
+                alert(json.password);
                 window.location = "success.html"; // Redirecting to other page.
                 return false;
             } else {
-                attempt -= 1;// Decrementing by one.
-                alert("You have left " + attempt + " attempts left!");
+                attempt -= 1;// Decrementing by one
+                //alert("You have left " + attempt + " attempts left!");
                 // Disabling fields after 3 attempts.
                 if (attempt === 0) {
                     document.getElementById("username").disabled = true;
@@ -32,7 +36,7 @@ function validate() {
     };
 
     http.open('POST', 'login.php', true);
-    http.send(userPass);
+    http.send(JSON.stringify(userPass));
     
 //    if (username === "Formget" && password === "formget#123") {
 //        alert("Login successfully");
