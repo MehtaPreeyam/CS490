6e2195d73cb154a630877f930d2be856fc85d89b
Nikhil Vummadi <nrv26@njit.edu>
2018-09-25 15:51:04 -0400

    Updated the code to make it work

--- MiddleEnd/alpha.php
+++ MiddleEnd/alpha.php
@@ -1,52 +1,43 @@
 <?php
 
-function njit_login($user, $pass){
-	// user=UCID&pass=pass&uuid=0xACA021
+function authenticate($username, $password){
+
+	
 	$ch = curl_init();
 	$ar = array(
-	 	"user" => $user,
-	 	"pass" => $pass,
-	 	"uuid" => "0xACA021"
+	 	"ucid" => $username,
+	 	"pass" => $password
 	);
-	
+
 	curl_setopt_array($ch, array(
-		CURLOPT_URL => "https://cp4.njit.edu/cp/home/login",
+		CURLOPT_URL => "https://aevitepr2.njit.edu/myhousing/login.cfm",
 		CURLOPT_POSTFIELDS => http_build_query($ar),
 		CURLOPT_RETURNTRANSFER => 1
 	));
-	
 	$result = curl_exec($ch);
 	curl_close($ch);
-	// Logout to kill any sessions
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "http://cp4.njit.edu/up/Logout?uP_tparam=frm&frm=");
-	curl_exec($ch);
-	curl_close($ch);
 	// Return validation bool
-	return strpos($result, "loginok.html") !== false;
+	return strpos($result, "Please login using your UCID") != true;
 }
 
-
-$json_data = json_decode(file_get_contents("php://input"));
-$data = $json_data;
-$username = $data['username']
-$password = $data['password']
-if(njit_login($username, $password))
-	echo '{ "valid":"valid"}';
+$json_data = file_get_contents("php://input");
+$data = json_decode($json_data, true);
+$username = $data['username'];
+$password = $data['password'];
+if(authenticate($username, $password))
+  echo '{"valid":"valid"}';
 else{
-	$arr = array('username' => $username, 'password' => $password); 
 	$ch = curl_init();
 
 	curl_setopt_array($ch, array(
 		CURLOPT_RETURNTRANSFER => 1,
 		CURLOPT_URL => "https://web.njit.edu/~sk972/alpha.php",
-		CURLOPT_USERAGENT => "POST Request from middle",
+		CURLOPT_USERAGENT => "POST Request to back",
 		CURLOPT_POST => 1,
-		CURLOPT_POSTFIELDS => $arr
+		CURLOPT_POSTFIELDS => $json_data
 	));
-
+ 
 	$response = curl_exec($ch);
-
 	curl_close($ch);
 	echo $response;
 }
