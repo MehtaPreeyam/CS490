5bc6c62bfa13b6ec7a492a83045f303b3e2c41e6
Preeyam Mehta <preeyammehta12@gmail.com>
2018-09-25 11:37:29 -0400

    Front fixes

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,5 @@
 /*jslint devel: true */
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+var attempt = 3; 
 
 function validate() {
     'use strict';
@@ -13,16 +12,14 @@ function validate() {
     console.log(userPass);
     http.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
             var json = JSON.parse(this.responseText);
-            console.log(json);
-            alert(json.username);
-            if (json.username === 'ppm24') {
-                alert(json.password);
-                window.location = "success.html"; // Redirecting to other page.
+            if (json.valid === 'valid') {
+                document.body.innerHTML = 'Success!'; 
                 return false;
             } else {
                 attempt -= 1;// Decrementing by one
-                //alert("You have left " + attempt + " attempts left!");
+                alert("You have left " + attempt + " attempts left!");
                 // Disabling fields after 3 attempts.
                 if (attempt === 0) {
                     document.getElementById("username").disabled = true;
@@ -34,23 +31,6 @@ function validate() {
                 
         }
     };
-
     http.open('POST', 'login.php', true);
     http.send(JSON.stringify(userPass));
-    
-//    if (username === "Formget" && password === "formget#123") {
-//        alert("Login successfully");
-//        window.location = "success.html"; // Redirecting to other page.
-//        return false;
-//    } else {
-//        attempt -= 1;// Decrementing by one.
-//        alert("You have left " + attempt + " attempts left!");
-//        // Disabling fields after 3 attempts.
-//        if (attempt === 0) {
-//            document.getElementById("username").disabled = true;
-//            document.getElementById("password").disabled = true;
-//            document.getElementById("submit").disabled = true;
-//            return false;
-//        }
-//    }
 }
